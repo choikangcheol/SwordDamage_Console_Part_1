ca42c14f305ebe6d339f0d6ffcc4cd58c566adb7
choikangcheol <skynlake@gmail.com>
2025-01-08 18:23:58 +0900

    Feat: 코드 개선

--- SwordDamage_Console_Part_1/Program.cs
+++ SwordDamage_Console_Part_1/Program.cs
@@ -9,28 +9,22 @@ namespace SwordDamage_Console_Part_1
     internal class Program
     {
         static void Main(string[] args)
-        {            
+        {
             SwordDamage swordDamage = new SwordDamage();
             Random random = new Random();
 
             while (true)
             {
                 Console.Write("0 for no magic/flaming, 1 for magic, 2 for flaming, 3 for both, anything else to quit: ");
-                ConsoleKeyInfo keyInfo = Console.ReadKey(false);                
+                ConsoleKeyInfo keyInfo = Console.ReadKey(false);
 
                 switch (keyInfo.KeyChar)
                 {
                     case '0':
-                        CalculateSwordAttackPower(swordDamage, random, false, false);
-                        break;
                     case '1': // magic
-                        CalculateSwordAttackPower(swordDamage, random, true, false);
-                        break;
                     case '2': // flaming
-                        CalculateSwordAttackPower(swordDamage, random, false, true);
-                        break;
                     case '3': // magic + flaming
-                        CalculateSwordAttackPower(swordDamage, random, true, true);
+                        CalculateSwordAttackPower(swordDamage, random, keyInfo.KeyChar);
                         break;
                     default:
                         return;
@@ -38,12 +32,12 @@ namespace SwordDamage_Console_Part_1
             }
         }
 
-        static void CalculateSwordAttackPower(SwordDamage swordDamage, Random random, bool magic, bool flaming)
+        static void CalculateSwordAttackPower(SwordDamage swordDamage, Random random, char input)
         {
             int Roll = RollThreeDice(random);
             swordDamage.SetRoll(Roll);
-            swordDamage.SetMagic(magic);
-            swordDamage.SetFlaming(flaming);
+            swordDamage.SetMagic(input == '1' || input == '3');
+            swordDamage.SetFlaming(input == '2' || input == '3');
             Console.WriteLine($"\n{swordDamage.ToString()}\n");
         }
 
